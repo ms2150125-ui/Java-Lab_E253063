a1e80d9897d58d4ba74e0efcc4737013f145cebc
ms2150125-ui <ms2150125@gmail.com>
2026-06-30 22:45:24 +0600

    Add files via upload

---
+++ Java Lab final git/Lab_one/Task 1/Info.java
@@ -0,0 +1,19 @@
+public class Info {
+
+    public static void main(String[] args) {
+
+        String name = "Mojibur Ramen";
+        String studentID = "E253040";
+        int age = 19;
+        String phone = "01880482088";
+        String email = "mojib225899@gmail.com";
+        String address = "Cox's Bazar, Chittagong";
+
+        System.out.println("Name: " + name);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Age: " + age);
+        System.out.println("Phone: " + phone);
+        System.out.println("Email: " + email);
+        System.out.println("Address: " + address);
+    }
+}
---
+++ Java Lab final git/lab 9/Task 2/Lab9Task2.java
@@ -0,0 +1,56 @@
+class BankAccount {
+    private String accountHolder;
+    private String accountNumber;
+    private double balance;
+
+    public void setAccountHolder(String name) {
+        accountHolder = name;
+    }
+
+    public void setAccountNumber(String number) {
+        accountNumber = number;
+    }
+
+    public void setBalance(double amount) {
+        if (amount >= 0) {
+            balance = amount;
+        }
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void showAccountInfo() {
+        System.out.println("Account Holder: " + accountHolder);
+        System.out.println("Account Number: " + maskAccountNumber(accountNumber));
+        System.out.println("Balance: " + balance);
+    }
+
+    private String maskAccountNumber(String number) {
+        if (number.length() <= 4) {
+            return number;
+        }
+        return "****" + number.substring(number.length() - 4);
+    }
+}
+
+public class Lab9Task2 {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount();
+        account.setAccountHolder("Info");
+        account.setAccountHolder("Mojibur Ramen");
+        account.setAccountNumber("1234567890");
+        account.setBalance(5000);
+
+        account.showAccountInfo();
+    }
+}
